df1e88529ea591a0f92f4fb0ffd6ea4c74072f37
David Normandin <dvdnormans@hotmail.com>
2022-11-14 04:40:59 -0500

    (Labo2) second_exercice completed

--- Labo2/src/Premier_Exercice.java
+++ Labo2/src/Premier_Exercice.java
@@ -35,14 +35,17 @@ public class Premier_Exercice {
     	// écrire notre code pour que nous implémentions le prochain mouvement (au départ 
     	// le premier mouvement) du disque 3 en premier récursivement. Il sera la base de 
     	// notre pile grandissante, laquelle comportera également le prochain 
-    	// (donc même chose au départ le premier) mouvement du disque 2 au milieu de la pile et 
+    	// (au départ le premier) mouvement du disque 2 au milieu de la pile et 
     	// finalement le prochain mouvement du disque 1, ce dernier étant au sommet de la pile 
-    	// et sera donc notre premier print de la partie.
+    	// il sera donc notre premier mouvement de la partie.
+    	
+		// disque avec lequel on effectue le déplacement ---> paramètre 1
+		// emplacement présent du disque -------------------> paramètre 2
+		// destination du disque ---------------------------> paramètre 3
+		// tour non-utilisée pour le déplacement -----------> paramètre 4
     	
     	// Notre premier appel à toursDeHanoi sera donc:
         toursDeHanoi(N, 'A', 'C', 'B');
-        // car le premier mouvement (et le seul avec un jeu de 3 disque) du plus gros disque 
-        // N (3) doit former le premier appel à toursDeHanoi. 
     }
     
 	public static void toursDeHanoi(int n, 
@@ -52,32 +55,27 @@ public class Premier_Exercice {
 		// Condition d'arrêt de la récursion (il n'y a pas de disque 0)
 		if(n == 0) {
 			return;
-		}		
-		// disque avec lequel on effectue le déplacement ---> paramètre 1
-		// emplacement présent du disque -------------------> paramètre 2
-		// destination du disque ---------------------------> paramètre 3
-		// tour non-utilisée pour le déplacement -----------> paramètre 4
-		
-		// On empile dans le call stack jusqu'à ce que nous soyons à notre condition
-		// d'arrêt.
+		}				
+		// On empile jusqu'à ce que nous soyons à notre condition d'arrêt.
 		toursDeHanoi(n-1, tourEmplacementInitial, tourAuxiliaire, tourNouvelEmplacement);
-		// Exemple: État du premier call stack (avant les déplacements simulés par le print):
+		// Exemple: État du premier call stack (avant les mouvements simulés par le print):
 		// 			Pile_Base = (3, A, C, B)
 		// 			Pile 2 =    (2, A, B, C)
 		// 			Pile_Top =  (1, A, C, B)
 		System.out.println(" Disque #"+n+" déplacé de la tour "+tourEmplacementInitial+
 				           " vers la tour "+tourNouvelEmplacement);
-		// On rappelle toursDeHanoi pour préparer le prochain mouvement du disque N-1 
-		// (où N représente le disque qui vient d'être déplacé) 
+		// Nouvelle appel à toursDeHanoi pour préparer le prochain mouvement du disque N-1 
+		// (où N représente le disque venant d'être déplacé) 
 		// OU
 		// pour dynamiquement ajuster le call stack si N était le disque 1
+		// (c'est le cas au tout début de la partie lorsque l'on déplace le disque 1 au
+		// sommet de notre pile originale)
 		toursDeHanoi(n-1, tourAuxiliaire, tourNouvelEmplacement, tourEmplacementInitial);
 	}
 	
-	// Analyse du BigO = À chaque appel récursif s'empilant dans le call stack, 
-	//                   nous avons 2 possibilités: L'algorithme se rappelle et
-    //                   grossi la pile s'il passe le test de la condition d'arrêt
-	//                   ou le thread se termine via la condition d'arrêt.
-	//                   donc: (2 x 2 x 2 x ... x 2) x N = 2^N
+	// Analyse du BigO = À chaque appel récursif nous avons 2 possibilités: 
+	//                   L'algorithme se rappelle lui-même ou le thread actuel se 
+	//                   termine via la condition d'arrêt.
+	//                   donc: (2 x 2 x 2 x ...) = 2^N
 	
 }
